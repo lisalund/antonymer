dc49ccb462f8a051e5d16f348656e9e25484136f
lisalund <lisa.lund93@gmail.com>
2016-03-16 21:44:44 +0100

    Added a login dummy page. Also made things look pretty :)

--- antonyms/urls.py
+++ antonyms/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name = 'index'),
+    url(r'^login', views.login, name='login'),
 ]
--- antonyms/views.py
+++ antonyms/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.http import HttpResponse
-
+#from django.contrib.auth.decorators import login_required #Adds a decorator for pages that need login. If the user isn't logged in, take them to login page before.
+# to make use of login_required: add @login_required above the def-line for view.
 
 def index(request):
 	return render(request, 'antonyms/index.html', {})
+
+
+def login(request):
+	return render(request, 'antonyms/login.html', {})
