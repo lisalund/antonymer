d8272cdd62af70769b5ff44aa36ab4d81bd359c8
finbel <oregan@kth.se>
2016-03-24 09:12:57 +0100

    added login

--- antonymapp/views.py
+++ antonymapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required #Adds a decorator for pages that need login. If the user isn't logged in, take them to login page before.
+# to make use of login_required: add @login_required above the def-line for view.
 
-# Create your views here.
+def index(request):
+	return render(request, 'antonyms/index.html', {})
+
+
+def login(request):
+	return render(request, 'templates/login.html', {})
+
+@login_required
+def play(request):
+	return render(request, 'antonyms/play.html', {})
+
+def highscores(request):
+	return render(request, 'antonyms/highscores.html', {})
