fbde31cfbf4843a8e93998dc9085c5af7c261be8
finbel <oregan@kth.se>
2016-03-30 10:12:21 +0200

    setup create user function

--- antonymapp/urls.py
+++ antonymapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^play', views.play, name='play'),
     url(r'^highscores', views.highscores, name='highscores'),
+    url(r'^create_user/$', views.create_user, name='create_user'),
 ]
--- antonymapp/views.py
+++ antonymapp/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required #Adds a decorator for pages that need login. If the user isn't logged in, take them to login page before.
 # to make use of login_required: add @login_required above the def-line for view.
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 def index(request):
 	return render(request, 'antonymapp/index.html', {})
@@ -10,6 +13,19 @@ def index(request):
 
 def login(request):
 	return render(request, 'antonymapp/login.html', {})
+
+def create_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            new_user = User.objects.create_user(username=username, password=password)
+            return redirect('play')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'antonymapp/create_user.html', {'form': form}) 
 
 @login_required
 def play(request):
