3eb9064e7651a808488deb311dafc5d44bdb2284
lisalund <lisa.lund93@gmail.com>
2016-03-31 17:20:38 +0200

    fixed bug in models, and fixed words to radio buttons

--- antonymapp/forms.py
+++ antonymapp/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class SomeForm(forms.Form):
-    CHOICES = ((1,'1'),
-               (2,'2'),
-               (3,'3'),
-               (4,'4'),
-               (5,'5'),)
+    CHOICES = ((1,'Usla'),
+               (2,'Dåliga'),
+               (3,'Okej'),
+               (4,'Rätt bra'),
+               (5,'Jättebra'),)
     picked = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect())
